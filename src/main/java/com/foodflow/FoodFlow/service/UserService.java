25484f574cfc43a097219c781b43c9df64a9d8ae
Thalles Borges <dev.thallesborges@gmail.com>
2026-04-15 10:32:04 -0300

    feat: add password hashing on user creation

--- src/main/java/com/foodflow/FoodFlow/service/UserService.java
+++ src/main/java/com/foodflow/FoodFlow/service/UserService.java
@@ -6,22 +6,26 @@ import com.foodflow.FoodFlow.dto.CreateUserRequest;
 import com.foodflow.FoodFlow.dto.UserResponse;
 import lombok.RequiredArgsConstructor;
 import org.apache.catalina.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public UserResponse createUser(CreateUserRequest createUserRequest) {
         if (userRepository.existsByEmail(createUserRequest.getEmail())) {
             throw new RuntimeException("Email já cadastrado");
         }
 
+        String senhaHash = passwordEncoder.encode(createUserRequest.getSenha());
+
         UserEntity newUser = UserEntity.builder()
                 .nome(createUserRequest.getNome())
                 .email(createUserRequest.getEmail())
-                .senha(createUserRequest.getSenha())
+                .senha(senhaHash)
                 .build();
 
         UserEntity savedUser = userRepository.save(newUser);
