f5d33393e9dd0c37b446731b22a9fea96216df5a
Thalles Borges <dev.thallesborges@gmail.com>
2026-04-18 00:19:38 -0300

    test: configure testcontainers for integration testing

--- src/test/java/com/foodflow/FoodFlow/FoodFlowApplicationTests.java
+++ src/test/java/com/foodflow/FoodFlow/FoodFlowApplicationTests.java
@@ -2,12 +2,14 @@ package com.foodflow.FoodFlow;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class FoodFlowApplicationTests {
 
 	@Test
-	void contextLoads() {
+	 void contextLoads() {
 	}
 
 }
